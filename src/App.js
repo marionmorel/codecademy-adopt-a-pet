ede52ea6d8ab6fa4e5eb20f458517f04281ae41b
Marion Morel <marion.mrl@live.fr>
2024-04-17 14:31:50 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,9 +3,9 @@ import SearchPage from './pages/search';
 import PetDetailsPage from './pages/detail';
 import PetDetailsNotFound from './pages/petDetailsNotFound';
 import Root from './components/root';
-import { RouterProvider, createBrowserRouter, createRoutesFromElements, Route } from 'react-router-dom';
 
 // Add react-router-dom imports
+import { RouterProvider, createBrowserRouter, createRoutesFromElements, Route } from 'react-router-dom';
 
 // create router with JSX Route elements
 const appRouter = "REPLACE ME";
